e8098efd8a5f0a9878c8f179c653afb2cfa48b11
Skyler Cheung <skcheung@seas.upenn.edu>
2024-03-07 19:09:45 -0500

    user auth – button

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen />
+          <Modal actionLabel="Submit" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
